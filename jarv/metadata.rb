90dca470d72dc7be56a3fb52f3789607633d41a7
John Jarvis <johnjarv@amazon.com>
2015-03-03 13:40:58 +0100

    updating metadata

--- jarv/metadata.rb
+++ jarv/metadata.rb
@@ -5,3 +5,6 @@ license          'All rights reserved'
 description      'Installs/Configures jarv'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+dependency       'python'
+dependency       'apt'
+
