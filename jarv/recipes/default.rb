94e438a23f97ac8b5d60d2b2c0106fe1d805f365
John Jarvis <johnjarv@amazon.com>
2015-03-03 12:34:05 +0100

    removing dependencies

--- jarv/recipes/default.rb
+++ jarv/recipes/default.rb
@@ -10,4 +10,14 @@
 include_recipe 'python'
 include_recipe 'python::pip'
 
+# install ansible using pip
 python_pip "ansible"
+
+# install puppet using ppa
+
+apt_repository "puppet" do
+  distribution node['lsb']['codename']
+  uri "https://apt.puppetlabs.com"
+  keyserver "pool.sks-keyservers.net"
+  key "4BD6EC30"
+end
